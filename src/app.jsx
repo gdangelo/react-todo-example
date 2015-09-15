6dd2809ab31d9dad75af374c6a3e01fa3c69287b
Grégory D'Angelo <dangelo.gregory@gmail.com>
2015-09-15 18:28:23 +0200

    Render list of items

--- src/app.jsx
+++ src/app.jsx
@@ -2,14 +2,22 @@ var React = require('react');
 var ReactFire = require('reactfire');
 var Firebase = require('firebase');
 var Header = require('./header');
+var List = require('./list');
 
 var rootUrl = 'https://torrid-inferno-1049.firebaseio.com/';
 
 var App = React.createClass({
   mixins: [ ReactFire ],
+  getInitialState: function () {
+    return {
+      items: {},
+      loaded: false
+    };
+  },
   componentWillMount: function () {
     this.fb = new Firebase(rootUrl + 'items/');
     this.bindAsObject(this.fb, 'items');
+    this.fb.on('value', this.handleDataLoaded);
   },
   render: function () {
     return (
@@ -17,9 +25,15 @@ var App = React.createClass({
         <div className="col-md-8 col-md-offset-2">
           <h2 className="text-center">React Todo Example</h2>
           <Header itemsStore={this.firebaseRefs.items} />
+          <div className={"content " + (this.state.loaded ? "loaded" : "") }>
+            <List items={this.state.items} />
+          </div>
         </div>
       </div>
     )
+  },
+  handleDataLoaded: function () {
+    this.setState({loaded: true});
   }
 });
 
---
+++ src/list.jsx
@@ -0,0 +1,32 @@
+var React = require('react');
+
+module.exports = React.createClass({
+  render: function () {
+    return (
+      <ul>
+        {this.renderList()}
+      </ul>
+    );
+  },
+  renderList: function () {
+    if(this.props.items && Object.keys(this.props.items).length === 0){
+      return <h4>
+        Add a todo to get started
+      </h4>
+    } else {
+      var children = [];
+
+      for(key in this.props.items) {
+        if(this.props.items[key].text){
+          children.push(
+            <li key={key}>
+              {this.props.items[key].text}
+            </li>
+          );
+        }
+      }
+
+      return children;
+    }
+  }
+});
