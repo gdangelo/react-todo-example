a5d04f1633bb6ad3dff48147166dc4ba21ee2ae3
Grégory D'Angelo <dangelo.gregory@gmail.com>
2015-09-15 05:03:43 +0200

    Use Firebase as remotet datastore

--- src/app.jsx
+++ src/app.jsx
@@ -1,12 +1,21 @@
 var React = require('react');
+var ReactFire = require('reactfire');
+var Firebase = require('firebase');
+var rootUrl = 'https://torrid-inferno-1049.firebaseio.com/';
 
-var Hello = React.createClass({
+var App = React.createClass({
+  mixins: [ ReactFire ],
+  componentWillMount: function () {
+    this.fb = new Firebase(rootUrl + 'items/');
+    this.bindAsObject(this.fb, 'items');
+  },
   render: function () {
-    return <h1>
-      Hello, World!
-    </h1>
+    console.log(this.state);
+    return (
+      <h1>Hello, React!</h1>
+    )
   }
 });
 
-var element = React.createElement(Hello, {});
+var element = React.createElement(App, {});
 React.render(element, document.querySelector('.container'));
